49fc097e021335e1a5b4e051f4ad969f384d8cd6
VishwaSid2 <85431725+VishwaSid2@users.noreply.github.com>
2023-07-26 14:39:56 +0530

    Add files via upload

--- My try/script.js
+++ My try/script.js
@@ -9,16 +9,18 @@ const data = {
   score: document.querySelector(".score"),
   highScore: document.querySelector(".high-score"),
 };
-let math = Math.trunc(Math.random() * 20 + 1);
+const val = 20;
+let math = Math.trunc(Math.random() * val + 1);
 // data.answer.textContent = math;
 // Utilities
-let score = 20;
+document.querySelector(".again + p").textContent = `(Between 1 and ${val})`;
+let score = val;
 let highScore = 0;
 
+function message(text) {
+  return (data.message.textContent = text);
+}
 function activehandler() {
-  function message(text) {
-    return (data.message.textContent = text);
-  }
   const input = data.input.value;
   if (!input) {
     message("💥 Invalid Number");
@@ -29,13 +31,14 @@ function activehandler() {
       score = 0;
       data["body"].style.backgroundColor = "purple";
       message("...GAME OVER:(...");
+      data.check.style.visibility = "hidden";
       document.querySelector(".title-cont").textContent = "YOU LOOSE PLAYER!";
     }
     data.score.textContent = score;
   } else if (math == input) {
     data["body"].style.backgroundColor = "dodgerblue";
     data.answer.textContent = math;
-    score > highScore ? (highScore = score) : (highScore);
+    score > highScore ? (highScore = score) : highScore;
     message("YOU WON ");
     document.querySelector(".title-cont").textContent = "YOU WON PLAYER!";
     data.check.style.visibility = "hidden";
@@ -46,9 +49,9 @@ function activehandler() {
 
 function reset() {
   data.again.style.animationName = "";
-  score = 20;
+  score = val;
   data.score.textContent = score;
-  math = Math.trunc(Math.random() * 20 + 1);
+  math = Math.trunc(Math.random() * val + 1);
   message("Guess The Number!");
   data["body"].style.backgroundColor = "#222";
   data.check.style.visibility = "visible";
